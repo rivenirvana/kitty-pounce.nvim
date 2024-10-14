c6a1cdca9be3fc41a41da011897b8efc7a3971a5
Arvin Verain <arvinverain@proton.me>
2024-10-15 02:30:46 +0800

    feat: Add logic for focusing edge windows on VimEnter

--- lua/kitty-pounce/init.lua
+++ lua/kitty-pounce/init.lua
@@ -3,8 +3,8 @@ local M = {}
 
 local kitty_mappings = {
   h = 'left',
-  j = 'bottom',
-  k = 'top',
+  j = 'down',
+  k = 'up',
   l = 'right',
 }
 
@@ -16,7 +16,13 @@ local nvim_bindings = {
 }
 
 local function navigate(direction)
-  local neighbor = vim.fn.winnr('1' .. direction)
+  local neighbor = 1
+  if kitty_mappings[direction] then
+    neighbor = vim.fn.winnr('1' .. direction)
+  else
+    neighbor = vim.fn.winnr '$'
+  end
+
   if vim.fn.winnr() ~= neighbor or not vim.api.nvim_win_get_config(0).zindex then
     vim.api.nvim_command('wincmd ' .. direction)
   else
@@ -38,6 +44,28 @@ function M.setup()
   vim.api.nvim_create_user_command('NavigateRight', function()
     navigate 'l'
   end, {})
+  vim.api.nvim_create_user_command('NavigateUpLeft', function()
+    navigate 't'
+  end, {})
+  vim.api.nvim_create_user_command('NavigateBottomRight', function()
+    navigate 'b'
+  end, {})
+
+  vim.api.nvim_create_autocmd('FocusGained', {
+    callback = function()
+      vim.api.nvim_set_keymap('n', nvim_bindings.Left, ':NavigateUpLeft<CR>', {})
+      vim.api.nvim_set_keymap('n', nvim_bindings.Down, ':NavigateBottomRight<CR>', {})
+      vim.api.nvim_set_keymap('n', nvim_bindings.Up, ':NavigateUpLeft<CR>', {})
+      vim.api.nvim_set_keymap('n', nvim_bindings.Right, ':NavigateBottomRight<CR>', {})
+
+      vim.defer_fn(function()
+        vim.api.nvim_set_keymap('n', nvim_bindings.Left, ':NavigateLeft<CR>', { silent = true })
+        vim.api.nvim_set_keymap('n', nvim_bindings.Down, ':NavigateDown<CR>', { silent = true })
+        vim.api.nvim_set_keymap('n', nvim_bindings.Up, ':NavigateUp<CR>', { silent = true })
+        vim.api.nvim_set_keymap('n', nvim_bindings.Right, ':NavigateRight<CR>', { silent = true })
+      end, 5)
+    end,
+  })
 
   -- TODO: Make keybinds configurable
   vim.api.nvim_set_keymap('n', nvim_bindings.Left, ':NavigateLeft<CR>', { silent = true })
